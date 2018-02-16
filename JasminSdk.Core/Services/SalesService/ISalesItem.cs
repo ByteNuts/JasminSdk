312377d5f209f18fdd6cbaf561ad5fc2538ef64a
João Pereira <joaopereira@bytenuts.com>
2018-02-16 11:01:57 +0000

    Finished SalesItem Sales endpoints.

--- JasminSdk.Core/Services/SalesService/ISalesItem.cs
+++ JasminSdk.Core/Services/SalesService/ISalesItem.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using ByteNuts.PrimaveraBss.JasminSdk.Core.Models;
 using ByteNuts.PrimaveraBss.JasminSdk.Core.Models.Sales.SalesItem;
 
@@ -8,7 +9,97 @@ namespace ByteNuts.PrimaveraBss.JasminSdk.Core.Services.SalesService
     {
         #region GET
 
-        Task<ApiResponse<BaseSalesItemResource>> GetSalesItem(string itemKey);
+        /// <summary>
+        /// Returns all the resources available.<para/>
+        /// Response: When successful, the response body contains the list of resources available. See SalesItemResource.<para/>
+        /// Response status codes:<para/>
+        /// 200	OK. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.
+        /// </summary>
+        /// <returns>ApiResponse&lt;List&lt;SalesItemResource&gt;&gt;</returns>
+        Task<ApiResponse<List<SalesItemResource>>> GetSalesItems();
+
+        /// <summary>
+        /// Returns all the resources available.<para/>
+        /// Response: When successful, the response body contains the list of resources available. See SalesItemResource.<para/>
+        /// Response status codes:<para/>
+        /// 200	OK. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.
+        /// </summary>
+        /// <param name="page">The index of the page of the list that should be returned. This is a query string parameter.</param>
+        /// <param name="pageSize">The number of page elements that should be returned (max. is 1000). This is a query string parameter.</param>
+        /// <returns>ApiResponse&lt;List&lt;SalesItemResource&gt;&gt;</returns>
+        Task<ApiResponse<List<SalesItemResource>>> GetSalesItems(int page, int pageSize);
+
+        /// <summary>
+        /// Returns the specified resource.<para/>
+        /// Response: When successful, the response body contains the resource. See SalesItemResource.<para/>
+        /// Response status codes:<para/>
+        /// 200	OK. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.<para/>
+        /// 404	NotFound. The specified resource does not exist.
+        /// </summary>
+        /// <param name="id">The resource identifier.</param>
+        /// <returns>ApiResponse&lt;SalesItemResource&gt;</returns>
+        Task<ApiResponse<SalesItemResource>> GetSalesItemByIdentifier(string id);
+
+        /// <summary>
+        /// Returns the specified resource.<para/>
+        /// Response: When successful, the response body contains the resource. See SalesItemResource.<para/>
+        /// Response status codes:<para/>
+        /// 200	OK. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.<para/>
+        /// 404	NotFound. The specified resource does not exist.
+        /// </summary>
+        /// <param name="itemKey">The key for the item. Uniquely identifies the item within the organization.</param>
+        /// <returns>&lt;ApiResponse&lt;SalesItemResource&gt;</returns>
+        Task<ApiResponse<SalesItemResource>> GetSalesItemByKey(string itemKey);
+
+        /// <summary>
+        /// Returns all the resources available.<para/>
+        /// Response: When successful, the response body contains the resources. See BaseSalesItemResource.<para/>
+        /// Response status codes:<para/>
+        /// 200	OK. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.
+        /// </summary>
+        /// <returns>ApiResponse&lt;List&lt;BaseSalesItemResource&gt;&gt;</returns>
+        Task<ApiResponse<List<BaseSalesItemResource>>> GetSalesItemWithItem();
+
+        /// <summary>
+        /// Returns all the resources available.<para/>
+        /// Response: When successful, the response body contains the resources. See BaseSalesItemResource.<para/>
+        /// Response status codes:<para/>
+        /// 200	OK. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.
+        /// </summary>
+        /// <param name="page">The index of the page of the list that should be returned. This is a query string parameter.</param>
+        /// <param name="pageSize">The number of page elements that should be returned (max. is 1000). This is a query string parameter.</param>
+        /// <returns>ApiResponse&lt;List&lt;BaseSalesItemResource&gt;&gt;</returns>
+        Task<ApiResponse<List<BaseSalesItemResource>>> GetSalesItemWithItem(int page, int pageSize);
+
+        /// <summary>
+        /// Returns the specified resource.<para/>
+        /// Response: When successful, the response body contains the resource. See BaseSalesItemResource.<para/>
+        /// Response status codes:<para/>
+        /// 200	OK. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.<para/>
+        /// 404	NotFound. The specified resource does not exist.
+        /// </summary>
+        /// <param name="id">The identifier of the resource that should be returned.</param>
+        /// <returns>ApiResponse&lt;BaseSalesItemResource&gt;</returns>
+        Task<ApiResponse<BaseSalesItemResource>> GetSalesItemWithItemByIdentifier(string id);
+
+        /// <summary>
+        /// Returns the specified resource.<para/>
+        /// Response: When successful, the response body contains the resource. See BaseSalesItemResource.<para/>
+        /// Response status codes:<para/>
+        /// 200	OK. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.<para/>
+        /// 404	NotFound. The specified resource does not exist.
+        /// </summary>
+        /// <param name="itemKey">The key for the item. Uniquely identifies the item within the organization.</param>
+        /// <returns>ApiResponse&lt;BaseSalesItemResource&gt;</returns>
+        Task<ApiResponse<BaseSalesItemResource>> GetSalesItemWithItemByKey(string itemKey);
 
         #endregion GET
 
@@ -16,9 +107,39 @@ namespace ByteNuts.PrimaveraBss.JasminSdk.Core.Services.SalesService
 
         #region POST
 
-        Task<ApiResponse<string>> PostSalesItem(ApiBaseSalesItemResource salesItem);
+        /// <summary>
+        /// Creates a new resource.<para/>
+        /// Response: When successful, the response body contains the new resource identifier. The resource header location property contains the URL for the new resource. See ApiSalesItemResource.<para/>
+        /// Response status codes:<para/>
+        /// 201	Created. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.
+        /// </summary>
+        /// <param name="salesItemResource">The request body should contain the resource that should be created. See ApiSalesItemResource.</param>
+        /// <returns>ApiResponse&lt;string&gt;</returns>
+        Task<ApiResponse<string>> PostSalesItem(ApiSalesItemResource salesItemResource);
 
-        Task<ApiResponse<string>> PostSalesItemPriceListLine(string itemKey, ApiBaseSalesItemPriceListLinesResource priceListLine);
+        /// <summary>
+        /// Creates a new resource.<para/>
+        /// Response: When successful, the response body contains the new resource identifier. The resource header location property contains the URL for the new resource. See ApiBaseSalesItemResource.<para/>
+        /// Response status codes:<para/>
+        /// 201	Created. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.
+        /// </summary>
+        /// <param name="salesItemResource">The request body should contain the resource that should be created. See ApiSalesItemResource.</param>
+        /// <returns></returns>
+        Task<ApiResponse<string>> PostSalesItemWithApiBaseSalesItem(ApiBaseSalesItemResource salesItemResource);
+
+        /// <summary>
+        /// Inserts a new 'Price List Line' in the 'Sales Item'.<para/>
+        /// Response: When successful, the response body is empty.<para/>
+        /// Response status codes:<para/>
+        /// 204	NoContent. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.
+        /// </summary>
+        /// <param name="itemKey">The key for the item. Uniquely identifies the item within the organization.</param>
+        /// <param name="value">The ApiSalesItemPriceListLinesResource instance. See ApiSalesItemPriceListLinesResource.</param>
+        /// <returns></returns>
+        Task<ApiResponse<string>> PostSalesItemPriceListLine(string itemKey, ApiBaseSalesItemPriceListLinesResource value);
 
 
         #endregion POST
@@ -27,13 +148,163 @@ namespace ByteNuts.PrimaveraBss.JasminSdk.Core.Services.SalesService
 
         #region PUT
 
+        /// <summary>
+        /// Updates the attribute 'Currency' in the specified resource.<para/>
+        /// Response: When successful, the response body is empty.<para/>
+        /// Response status codes:<para/>
+        /// 200	OK. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.
+        /// </summary>
+        /// <param name="itemKey">The key for the item. Uniquely identifies the item within the organization.</param>
+        /// <param name="lineId">The detail identifier.</param>
+        /// <param name="value">The request body should contain the new value for the attribute.</param>
+        /// <returns></returns>
+        Task<ApiResponse<string>> PutSetCurrencyPriceListLine(string itemKey, string lineId, string value);
+
+        /// <summary>
+        /// Updates the attribute 'IncomeAccount' in the specified resource.<para/>
+        /// Response: When successful, the response body is empty.<para/>
+        /// Response status codes:<para/>
+        /// 200	OK. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.
+        /// </summary>
+        /// <param name="itemKey">The key for the item. Uniquely identifies the item within the organization.</param>
+        /// <param name="value">The request body should contain the new value for the attribute.</param>
+        /// <returns></returns>
+        Task<ApiResponse<string>> PutSetIncomeAccountSalesItem(string itemKey, string value);
+
+        /// <summary>
+        /// Updates the attribute 'IsActive' in the specified resource.<para/>
+        /// Response: When successful, the response body is empty.<para/>
+        /// Response status codes:<para/>
+        /// 200	OK. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.<para/>
+        /// 404	NotFound. The specified resource does not exist.
+        /// </summary>
+        /// <param name="itemKey">The key for the item. Uniquely identifies the item within the organization.</param>
+        /// <param name="value">The request body should contain the new value for the attribute.</param>
+        /// <returns></returns>
+        Task<ApiResponse<string>> PutSetIsActiveSalesItem(string itemKey, bool value);
+
+        /// <summary>
+        /// Updates the attribute 'ItemTaxSchema' in the specified resource.<para/>
+        /// Response: When successful, the response body is empty.<para/>
+        /// Response status codes:<para/>
+        /// 200	OK. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.
+        /// </summary>
+        /// <param name="itemKey">The key for the item. Uniquely identifies the item within the organization.</param>
+        /// <param name="value">The request body should contain the new value for the attribute.</param>
+        /// <returns></returns>
+        Task<ApiResponse<string>> PutSetItemTaxSchemaSalesItem(string itemKey, string value);
+
+        /// <summary>
+        /// Updates the attribute 'ItemWithholdingTaxSchema' in the specified resource.<para/>
+        /// Response: When successful, the response body is empty.<para/>
+        /// Response status codes:<para/>
+        /// 200	OK. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.
+        /// </summary>
+        /// <param name="itemKey">The key for the item. Uniquely identifies the item within the organization.</param>
+        /// <param name="value">The request body should contain the new value for the attribute.</param>
+        /// <returns></returns>
+        Task<ApiResponse<string>> PutSetItemWithholdingTaxSchemaSalesItem(string itemKey, string value);
+
+        /// <summary>
+        /// Updates the attribute 'PriceAmount' in the specified resource.<para/>
+        /// Response: When successful, the response body is empty.<para/>
+        /// Response status codes:<para/>
+        /// 200	OK. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.
+        /// </summary>
+        /// <param name="itemKey">The key for the item. Uniquely identifies the item within the organization.</param>
+        /// <param name="lineId">The detail identifier.</param>
+        /// <param name="value">The request body should contain the new value for the attribute.</param>
+        /// <returns></returns>
         Task<ApiResponse<string>> PutSetPriceAmountPriceListLine(string itemKey, string lineId, decimal value);
 
+        /// <summary>
+        /// Updates the attribute 'PriceList' in the specified resource.<para/>
+        /// Response: When successful, the response body is empty.<para/>
+        /// Response status codes:<para/>
+        /// 200	OK. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.
+        /// </summary>
+        /// <param name="itemKey">The key for the item. Uniquely identifies the item within the organization.</param>
+        /// <param name="lineId">The detail identifier.</param>
+        /// <param name="value">The request body should contain the new value for the attribute.</param>
+        /// <returns></returns>
         Task<ApiResponse<string>> PutSetPriceListPriceListLine(string itemKey, string lineId, string value);
 
-        Task<ApiResponse<string>> PutSetItemTaxSchema(string itemKey, string value);
+        /// <summary>
+        /// Updates the attribute 'Unit' in the specified resource.<para/>
+        /// Response: When successful, the response body is empty.<para/>
+        /// Response status codes:<para/>
+        /// 200	OK. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.
+        /// </summary>
+        /// <param name="itemKey">The key for the item. Uniquely identifies the item within the organization.</param>
+        /// <param name="lineId">The detail identifier.</param>
+        /// <param name="value">The request body should contain the new value for the attribute.</param>
+        /// <returns></returns>
+        Task<ApiResponse<string>> PutSetUnitPriceListLine(string itemKey, string lineId, string value);
+
+        /// <summary>
+        /// Updates the attribute 'Unit' in the specified resource.<para/>
+        /// Response: When successful, the response body is empty.<para/>
+        /// Response status codes:<para/>
+        /// 200	OK. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.
+        /// </summary>
+        /// <param name="itemKey">The key for the item. Uniquely identifies the item within the organization.</param>
+        /// <param name="value">The request body should contain the new value for the attribute.</param>
+        /// <returns></returns>
+        Task<ApiResponse<string>> PutSetUnitSalesItem(string itemKey, string value);
+
+
 
         #endregion PUT
 
+
+        #region DELETE
+
+        /// <summary>
+        /// Deletes the specified resource.<para/>
+        /// Response: When successful, the response body is empty.<para/>
+        /// Response status codes:<para/>
+        /// 204	NoContent. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.<para/>
+        /// 404	NotFound. The specified resource does not exist.
+        /// </summary>
+        /// <param name="itemKey">The key for the item. Uniquely identifies the item within the organization.</param>
+        /// <param name="lineId">The line identifier.</param>
+        /// <returns></returns>
+        Task<ApiResponse<string>> DeletePriceListLineByIdentifier(string itemKey, string lineId);
+
+        /// <summary>
+        /// Deletes the specified resource.<para/>
+        /// Response: When successful, the response body is empty.<para/>
+        /// Response status codes:<para/>
+        /// 204	Deleted. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.<para/>
+        /// 404	NotFound. The specified resource does not exist.
+        /// </summary>
+        /// <param name="id">The resource identifier.</param>
+        /// <returns></returns>
+        Task<ApiResponse<string>> DeleteSalesItemByIdentifier(string id);
+
+        /// <summary>
+        /// Deletes the specified resource.<para/>
+        /// Response: When successful, the response body is empty.<para/>
+        /// Response status codes:<para/>
+        /// 204	Deleted. The operation was completed successfully.<para/>
+        /// 400	BadRequest. The request parameters are invalid.<para/>
+        /// 404	NotFound. The specified resource does not exist.
+        /// </summary>
+        /// <param name="itemKey">The key for the item. Uniquely identifies the item within the organization.</param>
+        /// <returns></returns>
+        Task<ApiResponse<string>> DeleteSalesItemByKey(string itemKey);
+
+        #endregion DELETE
     }
 }
--- JasminSdk.Core/Services/SalesService/SalesItem.cs
+++ JasminSdk.Core/Services/SalesService/SalesItem.cs
@@ -1,4 +1,5 @@
-﻿using ByteNuts.PrimaveraBss.JasminSdk.Core.Helpers;
+﻿using System.Collections.Generic;
+using ByteNuts.PrimaveraBss.JasminSdk.Core.Helpers;
 using ByteNuts.PrimaveraBss.JasminSdk.Core.Models;
 using ByteNuts.PrimaveraBss.JasminSdk.Core.Models.Sales.SalesItem;
 using Newtonsoft.Json;
@@ -15,7 +16,64 @@ namespace ByteNuts.PrimaveraBss.JasminSdk.Core.Services.SalesService
 
         #region GET
 
-        public async Task<ApiResponse<BaseSalesItemResource>> GetSalesItem(string itemKey)
+
+        public async Task<ApiResponse<List<SalesItemResource>>> GetSalesItems()
+        {
+            var url = $"{EndPoint}/extension";
+
+            return await ApiCall<List<SalesItemResource>>.Get(url);
+        }
+
+
+        public async Task<ApiResponse<List<SalesItemResource>>> GetSalesItems(int page, int pageSize)
+        {
+            var url = $"{EndPoint}/extension?page={page}&pageSize={pageSize}";
+
+            return await ApiCall<List<SalesItemResource>>.Get(url);
+        }
+
+
+        public async Task<ApiResponse<SalesItemResource>> GetSalesItemByIdentifier(string id)
+        {
+            var url = $"{EndPoint}/{id}/extension";
+
+            return await ApiCall<SalesItemResource>.Get(url, true);
+        }
+
+
+        public async Task<ApiResponse<SalesItemResource>> GetSalesItemByKey(string itemKey)
+        {
+            var url = $"{EndPoint}/{itemKey}/extension";
+
+            return await ApiCall<SalesItemResource>.Get(url, true);
+        }
+
+
+        public async Task<ApiResponse<List<BaseSalesItemResource>>> GetSalesItemWithItem()
+        {
+            var url = $"{EndPoint}";
+
+            return await ApiCall<List<BaseSalesItemResource>>.Get(url);
+        }
+
+
+        public async Task<ApiResponse<List<BaseSalesItemResource>>> GetSalesItemWithItem(int page, int pageSize)
+        {
+            var url = $"{EndPoint}?page={page}&pageSize={pageSize}";
+
+            return await ApiCall<List<BaseSalesItemResource>>.Get(url);
+        }
+
+
+        public async Task<ApiResponse<BaseSalesItemResource>> GetSalesItemWithItemByIdentifier(string id)
+        {
+            var url = $"{EndPoint}/{id}";
+
+            return await ApiCall<BaseSalesItemResource>.Get(url, true);
+        }
+
+
+        public async Task<ApiResponse<BaseSalesItemResource>> GetSalesItemWithItemByKey(string itemKey)
         {
             var url = $"{EndPoint}/{itemKey}";
 
@@ -30,23 +88,34 @@ namespace ByteNuts.PrimaveraBss.JasminSdk.Core.Services.SalesService
 
         #region POST
 
-        public async Task<ApiResponse<string>> PostSalesItem(ApiBaseSalesItemResource salesItem)
+        public async Task<ApiResponse<string>> PostSalesItem(ApiSalesItemResource salesItemResource)
         {
-            var url = $"{EndPoint}";
+            var url = $"{EndPoint}/extension";
 
-            var json = JsonConvert.SerializeObject(salesItem, RequestHelper.JsonSettings);
+            var json = JsonConvert.SerializeObject(salesItemResource, RequestHelper.JsonSettings);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
 
             return await ApiCall<string>.Post(url, content);
         }
 
 
-        public async Task<ApiResponse<string>> PostSalesItemPriceListLine(string itemKey, ApiBaseSalesItemPriceListLinesResource priceListLine)
+        public async Task<ApiResponse<string>> PostSalesItemWithApiBaseSalesItem(ApiBaseSalesItemResource salesItemResource)
+        {
+            var url = $"{EndPoint}";
+
+            var json = JsonConvert.SerializeObject(salesItemResource, RequestHelper.JsonSettings);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            return await ApiCall<string>.Post(url, content);
+        }
+
+
+        public async Task<ApiResponse<string>> PostSalesItemPriceListLine(string itemKey, ApiBaseSalesItemPriceListLinesResource value)
         {
             var url = $"{EndPoint}/{itemKey}/priceListLines";
 
 
-            var json = JsonConvert.SerializeObject(priceListLine, RequestHelper.JsonSettings);
+            var json = JsonConvert.SerializeObject(value, RequestHelper.JsonSettings);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
 
             return await ApiCall<string>.Post(url, content);
@@ -58,6 +127,61 @@ namespace ByteNuts.PrimaveraBss.JasminSdk.Core.Services.SalesService
 
 
         #region PUT
+
+        public async Task<ApiResponse<string>> PutSetCurrencyPriceListLine(string itemKey, string lineId, string value)
+        {
+            var url = $"{EndPoint}/{itemKey}/priceListLines/{lineId}/currency";
+
+            var json = JsonConvert.SerializeObject(value, RequestHelper.JsonSettings);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            return await ApiCall<string>.Put(url, content);
+        }
+
+
+        public async Task<ApiResponse<string>> PutSetIncomeAccountSalesItem(string itemKey, string value)
+        {
+            var url = $"{EndPoint}/{itemKey}/incomeAccount";
+
+            var json = JsonConvert.SerializeObject(value, RequestHelper.JsonSettings);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            return await ApiCall<string>.Put(url, content);
+        }
+
+
+        public async Task<ApiResponse<string>> PutSetIsActiveSalesItem(string itemKey, bool value)
+        {
+            var url = $"{EndPoint}/{itemKey}/isActive";
+
+            var json = JsonConvert.SerializeObject(value, RequestHelper.JsonSettings);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            return await ApiCall<string>.Put(url, content, true);
+        }
+
+
+        public async Task<ApiResponse<string>> PutSetItemTaxSchemaSalesItem(string itemKey, string value)
+        {
+            var url = $"{EndPoint}/{itemKey}/itemTaxSchema";
+
+            var json = JsonConvert.SerializeObject(value, RequestHelper.JsonSettings);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            return await ApiCall<string>.Put(url, content);
+        }
+
+
+        public async Task<ApiResponse<string>> PutSetItemWithholdingTaxSchemaSalesItem(string itemKey, string value)
+        {
+            var url = $"{EndPoint}/{itemKey}/itemWithholdingTaxSchema";
+
+            var json = JsonConvert.SerializeObject(value, RequestHelper.JsonSettings);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            return await ApiCall<string>.Put(url, content);
+        }
+
 
         public async Task<ApiResponse<string>> PutSetPriceAmountPriceListLine(string itemKey, string lineId, decimal value)
         {
@@ -79,9 +203,9 @@ namespace ByteNuts.PrimaveraBss.JasminSdk.Core.Services.SalesService
             return await ApiCall<string>.Put(url, content);
         }
 
-        public async Task<ApiResponse<string>> PutSetItemTaxSchema(string itemKey, string value)
+        public async Task<ApiResponse<string>> PutSetUnitPriceListLine(string itemKey, string lineId, string value)
         {
-            var url = $"{EndPoint}/{itemKey}/itemTaxSchema";
+            var url = $"{EndPoint}/{itemKey}/priceListLines/{lineId}/unit";
 
             var json = JsonConvert.SerializeObject(value, RequestHelper.JsonSettings);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
@@ -89,7 +213,50 @@ namespace ByteNuts.PrimaveraBss.JasminSdk.Core.Services.SalesService
             return await ApiCall<string>.Put(url, content);
         }
 
+
+        public async Task<ApiResponse<string>> PutSetUnitSalesItem(string itemKey, string value)
+        {
+            var url = $"{EndPoint}/{itemKey}/unit";
+
+            var json = JsonConvert.SerializeObject(value, RequestHelper.JsonSettings);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            return await ApiCall<string>.Put(url, content);
+        }
+
+
+
         #endregion PUT
 
+
+
+        #region DELETE
+
+
+        public async Task<ApiResponse<string>> DeletePriceListLineByIdentifier(string itemKey, string lineId)
+        {
+            var url = $"{EndPoint}/{itemKey}/priceListLines/{lineId}";
+
+            return await ApiCall<string>.Delete(url, true);
+        }
+
+
+        public async Task<ApiResponse<string>> DeleteSalesItemByIdentifier(string id)
+        {
+            var url = $"{EndPoint}/{id}";
+
+            return await ApiCall<string>.Delete(url, true);
+        }
+
+
+        public async Task<ApiResponse<string>> DeleteSalesItemByKey(string itemKey)
+        {
+            var url = $"{EndPoint}/{itemKey}";
+
+            return await ApiCall<string>.Delete(url, true);
+        }
+
+
+        #endregion DELETE
     }
 }
